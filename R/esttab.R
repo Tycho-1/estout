b15a8fc2680b860132cc0c944c64b661e8922757
Felix Kaminsky <fkamins@uni-goettingen.de>
2009-07-31 00:00:00 +0000

    version 0.9-1

--- R/esttab.R
+++ R/esttab.R
@@ -1,5 +1,5 @@
 `esttab` <-
-function(t.value=FALSE,p.value=FALSE,round.dec=3,caption=NULL,label=NULL,sig.levels=c(0.1,0.05,0.01),sig.sym=c("*","**","***"),filename="estout",csv=FALSE,sweave=FALSE,dcolumn=NULL,table="table",table.pos="htbp",caption.top=FALSE,booktabs=FALSE){
+function(t.value=FALSE,p.value=FALSE,round.dec=3,caption=NULL,label=NULL,sig.levels=c(0.1,0.05,0.01),sig.sym=c("*","**","***"),filename="estout",csv=FALSE,sweave=FALSE,dcolumn=NULL,table="table",table.pos="htbp",caption.top=FALSE,booktabs=FALSE,var.order=NULL,sub.sections=NULL,var.rename=NULL){
 
 # reading list from eststo
 model.coeff.list <<- ccl
@@ -19,6 +19,35 @@ if(is.null(caption)){texcaption <- caption}else{texcaption <- paste("\\caption{"
 # setting label if non-NULL
 if(is.null(label)){}else{label <- paste("\\label{tab:",label,"}\n",sep="")}
 
+# converting sub.sections vector to list of type list[i] = c(position,text)
+sub.sections.list <- NULL
+if(is.null(sub.sections) == FALSE){
+	if((length(sub.sections)%%2) == 0){
+		sub.sections.list  <- list() 
+		for(i in 1:(length(sub.sections)/2)){
+			sub.sections.list[[i]]  <- c(sub.sections[[2*i-1]], sub.sections[[2*i]])
+		}
+	}
+	else{
+		return(cat("Your 'sub.section' vector contains a mistake. Please check and run again."))
+	}
+}
+#print(sub.sections.list) #---- control
+
+# converting var.names vector to list of type list[i] = c(name.old,name.new)
+var.rename.list <- NULL
+if(is.null(var.rename) == FALSE){
+	if((length(var.rename)%%2) == 0){
+		var.rename.list  <- list() 
+		for(i in 1:(length(var.rename)/2)){
+			var.rename.list[[i]]  <- c(var.rename[[2*i-1]], var.rename[[2*i]])
+		}
+	}
+	else{
+		return(cat("Your 'var.rename' vector contains a mistake. Please check and run again."))
+	}
+}
+#print(var.rename.list)   #control --------!!!!!!!!!!!
 # setting TeX commands for booktabs
 if(booktabs == TRUE){
 	toprule <- "\\toprule\n"
@@ -61,22 +90,26 @@ else{
 	onestar <- paste("\\sym{",sig.sym[[1]],"}",sep="")
 }
 ######### creating a vector of variable names ##########################
-
-for (j in 1:length(model.coeff.list)){
-        col_length <- length(model.coeff.list[[j]]) # length of one model column list
-        for (i in 1:(col_length-1)){
-                var.count <- 0
-		for(var.exist in grepl(model.coeff.list[[j]][[i]][[1]],var_list)){
-			if(var.exist == FALSE){
-				var.count <- var.count + 1
-				
+if(is.null(var.order) == FALSE){
+	var_list <- var.order
+}
+else{
+	for (j in 1:length(model.coeff.list)){
+	        col_length <- length(model.coeff.list[[j]]) # length of one model column list
+	        for (i in 1:(col_length-1)){
+	                var.count <- 0
+			for(var.exist in grepl(model.coeff.list[[j]][[i]][[1]],var_list)){
+				if(var.exist == FALSE){
+					var.count <- var.count + 1
+					
+				}
+				else if(var.exist == TRUE){
+					break
+				}
 			}
-			else if(var.exist == TRUE){
-				break
+			if(var.count == length(var_list)){
+	                          var_list <- c(var_list,model.coeff.list[[j]][[i]][[1]])
 			}
-		}
-		if(var.count == length(var_list)){
-                          var_list <- c(var_list,model.coeff.list[[j]][[i]][[1]])
 		}
 	}
 }
@@ -165,26 +198,56 @@ for (j in 1:length(model.coeff.list)){
                                 output_matrix[k*2+1,j+1] <- std_err  #if set entry of std.err.
                         }
                 } # end for (i)
+#print(j) #control-----------------!!!!!!!!!!!
+#print(model.coeff.list[[j]][[col_length]][[1]]) #control-------------!!!!
+
+		# rename dep.var
+		dep.var <- deparse(ccl[[j]][[col_length]][[1]])
+		if(is.null(var.rename) == FALSE){
+			for(i in 1:length(var.rename.list)){
+				if(dep.var == var.rename.list[[i]][[1]]){
+					dep.var <- var.rename.list[[i]][[2]]
+				}
+			}
+		}
+print(is.null(var.rename)) #control ---------------!!!!!!!!!
+
+		#--------------------------------------------
+
                 output_matrix[1,1] <- " "
                 if(csv == TRUE){
-                output_matrix[1,j+1] <- paste(delimiter,model.coeff.list[[j]][[col_length]][[1]],sep="") #dep.var
+                	output_matrix[1,j+1] <- paste(delimiter,dep.var,sep="") #dep.var
                 }
                 else{
-                output_matrix[1,j+1] <- paste('&\\multicolumn{1}{c}{',model.coeff.list[[j]][[col_length]][[1]],"}",sep="") #dep.var
+	            	output_matrix[1,j+1] <- paste("&\\multicolumn{1}{c}{",dep.var,"}",sep="") #dep.var
                 }
                 if(model.coeff.list[[j]][[col_length]][[length(model.coeff.list[[j]][[col_length]])]] == "lm"){     # checking if model is of lm class
 #print("Model is of class 'lm'")
-                output_matrix[length(output_matrix[,j+1])-2,j+1] <- paste(delimiter,round(model.coeff.list[[j]][[col_length]][[2]],round.dec),sep="") # Rsquared
-                output_matrix[length(output_matrix[,j+1])-1,j+1] <- paste(delimiter,round(model.coeff.list[[j]][[col_length]][[3]],round.dec),sep="") # adj.Rsquared
-                output_matrix[length(output_matrix[,j+1]),j+1] <- paste(delimiter,"\\multicolumn{1}{c}{",model.coeff.list[[j]][[col_length]][[4]],"}",sep="") # N
+    	           	output_matrix[length(output_matrix[,j+1])-2,j+1] <- paste(delimiter,round(model.coeff.list[[j]][[col_length]][[2]],round.dec),sep="") # Rsquared
+        	    	output_matrix[length(output_matrix[,j+1])-1,j+1] <- paste(delimiter,round(model.coeff.list[[j]][[col_length]][[3]],round.dec),sep="") # adj.Rsquared
+                	output_matrix[length(output_matrix[,j+1]),j+1] <- paste(delimiter,"\\multicolumn{1}{c}{",model.coeff.list[[j]][[col_length]][[4]],"}",sep="") # N
                 }
                 if(model.coeff.list[[j]][[col_length]][[length(model.coeff.list[[j]][[col_length]])]] == "plm"){     # checking if model is of plm class
 #print("Model is of class 'plm'")
-                output_matrix[length(output_matrix[,j+1]),j+1] <- paste(delimiter,"\\multicolumn{1}{c}{",model.coeff.list[[j]][[col_length]][[2]],"}",sep="") # N
+                	output_matrix[length(output_matrix[,j+1]),j+1] <- paste(delimiter,"\\multicolumn{1}{c}{",model.coeff.list[[j]][[col_length]][[2]],"}",sep="") # N
                 }
 		#cat(output_matrix)  #control ----------!!!!!!!!!!
 } # end for model (j)
+#----------------------------------------------
 
+# replacing regression var names with real var names
+if(is.null(var.rename) == FALSE){
+	for(j in 2:length(output_matrix[,1])){
+		if(is.null(var.rename) == FALSE){
+			for(i in 1:length(var.rename.list)){
+				if(output_matrix[j,1] == var.rename.list[[i]][[1]]){
+					output_matrix[j,1] <- var.rename.list[[i]][[2]]
+				}
+			}
+		}
+	}
+}
+#----------------------------------------------
 
 if(csv == TRUE ){
 	sink(save.file)
@@ -206,10 +269,10 @@ else{
 # begin sink
 	sink(save.file)
 # collate TeX formatted table
-	cat(paste("\\def\\sym#1{\\ifmmode^{#1}\\else\\(^{#1}\\)\\fi}\n\\begin{",table,"}",table.pos, if(caption.top==TRUE){texcaption},"
+	cat("\\def\\sym#1{\\ifmmode^{#1}\\else\\(^{#1}\\)\\fi}\n\\begin{",table,"}",table.pos, if(caption.top==TRUE){texcaption},"
 \\centering
 \\begin{tabular}{l*{",om.ncol-2,"}{",dcolumn,"}}
-",toprule,sep=""))
+",toprule,sep="")
 	for (j in 1:om.nrow){
 	                if(j == 1){
 	                    cat(paste(paste("\t","&\\multicolumn{1}{c}{(",1:(om.ncol-2),")} ",collapse=" "),"\\\\",collapse=""))
@@ -221,6 +284,13 @@ else{
 	                if(j == (end.sep.line)){
 	                    cat(midrule)
 	                }
+			if(!is.null(sub.sections)){
+				for(k in 1:length(sub.sections.list)){
+					if(j == (as.integer(sub.sections.list[[k]][[1]])*2+1)){
+						cat("\\multicolumn{2}{c}{",sub.sections.list[[k]][[2]],"}",rep("&",om.ncol-3),"\\\\\n",sep="")
+					}
+				}
+			}
 	}
 	cat(bottomrule)
 	if(t.value==TRUE){
@@ -249,4 +319,3 @@ print(output_matrix) # console print-out
 }
 # esttab-end
 } 
-
